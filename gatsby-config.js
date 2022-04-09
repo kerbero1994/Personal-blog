d3383bc2d64518ac52631ba45f22b4c8ba57773f
Eduardo Gonzalez Zavala <eduardogonzalezzavala@MacBook-Air-de-Eduardo.local>
2022-04-08 20:22:14 -0500

    well another round of <this help with the issue>

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: "gatsby-plugin-material-ui",
       options: { stylesProvider: { injectFirst: true } },
     },
