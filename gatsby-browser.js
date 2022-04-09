a3c654aa3aa628ca8344f6266cdb10715659649b
Eduardo Gonzalez Zavala <eduardogonzalezzavala@MacBook-Air-de-Eduardo.local>
2022-04-08 21:09:36 -0500

    last attempt of the day

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,9 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+import ReactDOM from "react-dom"
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
